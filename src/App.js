3b10c4b349e1717ba63b296ad82272898ea16c62
Somanyu <somanyu.03samal@gmail.com>
2022-12-30 13:12:39 +0530

    🔧 plugin: Flowbite for components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-    <h1 className="text-3xl font-bold underline text-green-600">
-      Hello world!
-    </h1>
+      <Navbar />
+      <h1 className="text-3xl font-bold underline text-green-600">
+        Hello world!
+      </h1>
     </div>
   );
 }
